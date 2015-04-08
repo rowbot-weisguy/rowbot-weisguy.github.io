7576a51cf975cd5a745d7f95041ac6b9beb8c656
Rowan Weismiller <rowan.weismiller@gmail.com>
2015-04-07 23:31:08 -0700

    Brought music to the party

--- app.js
+++ app.js
@@ -47,16 +47,20 @@ ga('send', 'pageview');
 (function() {
     var body = document.querySelectorAll('body')[0];
     var partyButton = document.querySelectorAll('.party-button')[0];
+    var partyMusic = document.querySelectorAll('.party-music')[0];
     var isPartying = false;
 
     function stopParty() {
         body.classList.remove('party');
         partyButton.textContent = "Let's Party!";
+        partyMusic.pause();
+        partyMusic.currentTime = 0;
         isPartying = false;
     }
 
     function startParty() {
         body.classList.add('party');
+        partyMusic.play();
         partyButton.textContent = "Whoa, Calm Down";
         ga('send', 'event', 'Primary', 'Party Time');
         isPartying = true;
