6c959b10173cdf0d35e946c919a23139bed43221
Rowan Weismiller <rowan.weismiller@gmail.com>
2017-12-10 14:40:08 -0800

    Add gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `06gqdoo3hu2p`,
+        accessToken: `e29e422b90fb247049c9cd812bca9c0276dd28c1dde7bff00d3b104c20f8576f`,
+        host: `preview.contentful.com`,
+      },
+    },
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,48 @@
  */
 
  // You can delete this file if you're not using it
+
+ exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+
+  const pageQuery = `
+  query PageQuery {
+    allContentfulPage {
+      edges {
+        node {
+          id
+          title
+          slug
+          body
+        }
+      }
+    }
+  }
+  `
+
+  return new Promise((resolve, reject) => {
+    const templates = {
+      post: path.resolve(`src/layouts/post.js`),
+      page: path.resolve(`src/layouts/index.js`),
+    };
+
+    resolve(graphql(pageQuery).then(result => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+
+        const pages = result.data.allContentfulLandingPage.edges;
+
+        // Create pages for each markdown file.
+        pages.forEach(({ node }) => {
+
+          createPage({
+            path: node.slug,
+            component: templates.page,
+            context: node,
+          });
+        });
+      })
+    );
+  });
+};
